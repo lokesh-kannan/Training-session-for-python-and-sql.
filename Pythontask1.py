57573b6efe2ac9d11a7cd56834b9513932bf57c0
lokesh-kannan <161205796+lokesh-kannan@users.noreply.github.com>
2026-07-22 12:56:06 +0530

    completed task 1 in python

---
+++ Pythontask1.py
@@ -0,0 +1,92 @@
+# Here we are getting the input from the user and storing it in a variable
+Name = input("Enter your Name: ")
+College = input("Enter your College Name: ")
+Age = input("Enter your Age: ")
+'''
+Here we are printing all the user entered details
+print(Name)
+print(College)
+print(Age)
+'''
+print(Name)
+print(College)
+print(Age)
+
+StudentName = "Lokesh"
+StudenetCollege = "Prathyuysha Engineering College"
+StudentAge = 24
+StudentPercentage = 8.0
+City = "Chennai"
+StudentStatus = True
+print("StudentName is stored in the form of:")
+print(type(StudentName))
+print("Studentname: "+StudentName)
+print("StudentCollege is stored in the form of:")
+print(type(StudenetCollege))
+print("Studentcollege: "+StudenetCollege)
+print("StudentAge is stored in the form of:")
+print(type(StudentAge))
+print("StudentAge: "+str(StudentAge))
+print("StudentPercentage is stored in the form of:")
+print(type(StudentPercentage))
+print("Studentpercentage: "+str(StudentPercentage))
+print("Studentcity is stored in the form of:")
+print(type(City))
+print("Studentcity: "+City)
+print("Studentstatus is stored in the form of:")
+print(type(StudentStatus))
+print("Studentstaus: "+str(StudentStatus))
+
+
+print("The studentNAme is "+StudentName+" and the student is studying in the "+StudenetCollege+".")
+
+#Demonstrate type casting by performing the following:
+#Convert "100" to an integer.
+example1="100"
+print(type(example1))
+print(example1)
+change1=int(example1)
+print(type(change1))
+print(change1)
+
+#Convert 50 to a string.
+example2=50
+print(type(example2))
+print(example2)
+change2=str(example2)
+print(type(change2))
+print(change2)
+
+#Convert 25 to a float.
+example3=25
+print(type(example3))
+print(example3)
+change3=float(example3)
+print(type(change3))
+print(change3)
+
+#Convert 0 and 1 to boolean values.
+example4=0
+example5=1
+print(type(example4))
+print(example4)
+print(type(example5))
+print(example5)
+change4=bool(example4)
+print(type(change4))
+print(change4)
+change5=bool(example5)
+print(type(change5))
+print(change5)
+
+#valid variables
+StudentResult="pass"
+student1=False
+
+'''
+Invalid Variables
+25student="pass"
+print="lokesh"
+type=True
+True="Type"
+'''
