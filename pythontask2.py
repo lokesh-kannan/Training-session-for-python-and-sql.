6dc07010deb195b9948d197f37561fb06e14f99c
lokesh-kannan <lokeshkannangk@gmail.com>
2026-07-25 17:14:18 +0530

    task 2completed in python and commiting  the final code

---
+++ pythontask2.py
@@ -0,0 +1,103 @@
+a = 40
+b = 6
+
+# Addition
+c=a+b
+print("Addition of a+b: "+str(c))
+
+# subtraction
+d=a-b
+print("subtraction of a-b: "+str(d))
+
+# Multiplication
+e=a*b
+print("Multiplication of a*b: "+str(e))
+
+#division
+f=a/b
+print("division of a/b: "+str(f))
+
+#floor div
+g=a//b
+print("floor div of a//b: "+str(g))
+
+#Modules
+h=a%b
+print("Modules of a mod b: "+str(h))
+
+#Exponent
+i = a**b
+print("Exponent of a**b: "+str(i))
+
+# comparison operator
+
+print ("The comparison of a==b: "+str(a==b))
+
+print ("The comparison of a!=b: "+str(a!=b))
+
+print ("The comparison of a<b: "+str(a<b))
+
+print ("The comparison of a>b: "+str(a>b))
+
+print ("The comparison of a>=b: "+str(a>=b))
+
+print ("The comparison of a<=b: "+str(a<=b))
+
+#logical Operators
+
+x=True
+y=False
+
+print(x and y)
+
+print(x or y)
+
+print (not x)
+
+print(not y)
+
+#Assigment operator
+
+num = 25
+
+num+=5
+print (num)
+
+num-=3
+print (num)
+
+num*=2
+print(num)
+
+num/=4
+print(num)
+
+num//=2
+print(num)
+
+num%=5
+print(num)
+
+num**=2
+print(num)
+
+#bitwise operator
+
+a=12
+b=5
+#and
+print (a&b)
+#or
+print(a|b)
+
+print(a^b)
+#not
+print(~a)
+#left shift
+print(a<<2)
+#right shift
+print(a>>1)
+
+print(" a=12 "+" Binary representation of a: "+bin(a))
+
+print(" b=5 "+" Binary representation of b: "+bin(b))
